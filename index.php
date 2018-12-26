2274bb92126cb510468d724eda8ee65cc7312979
vagnernic <46137345+vagnernic@users.noreply.github.com>
2018-12-26 01:46:45 -0200

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -6,12 +6,13 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <title>TODO supply a title</title>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title></title>
     </head>
     <body>
-        <div>TODO write content </div></br>
-        
+        <?php
+        $ip = $_SERVER['REMOTE_ADDR'];
+        echo "eIP: ".$ip;
+        ?> 
     </body>
 </html>
